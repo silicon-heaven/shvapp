ca33ec0ee2267a5e90970e86b4d7502989b678ce
Fanda Vacek <vacek@elektroline.cz>
2017-12-15 10:05:52 +0100

    shvbroker default port changed to 3755

--- shvbroker/src/appclioptions.cpp
+++ shvbroker/src/appclioptions.cpp
@@ -5,7 +5,7 @@ AppCliOptions::AppCliOptions(QObject *parent)
 {
 	addOption("locale").setType(QVariant::String).setNames("--locale").setComment(tr("Application locale")).setDefaultValue("system");
 	addOption("profile").setType(QVariant::String).setNames("--profile").setComment("Server profile name").setDefaultValue("eyassrv");
-	addOption("server.port").setType(QVariant::Int).setNames("--server-port").setComment("Server port").setDefaultValue(3746);
+	addOption("server.port").setType(QVariant::Int).setNames("--server-port").setComment("Server port").setDefaultValue(3755);
 	addOption("sql.host").setType(QVariant::String).setNames("-s", "--sql-host").setComment("SQL server host");
 	addOption("sql.port").setType(QVariant::Int).setNames("--sql-port").setComment("SQL server port").setDefaultValue(5432);
 }
