ef2d96ece9cfd5761383bdddb78589694705246c
Fanda Vacek <vacek@elektroline.cz>
2018-02-02 13:46:50 +0100

    host, port CLI option added to shvagent

--- shvagent/src/appclioptions.h
+++ shvagent/src/appclioptions.h
@@ -12,5 +12,8 @@ private:
 public:
 	AppCliOptions(QObject *parent = NULL);
 	~AppCliOptions() Q_DECL_OVERRIDE {}
+
+	CLIOPTION_GETTER_SETTER2(int, "server.port", s, setS, erverPort)
+	CLIOPTION_GETTER_SETTER2(QString, "server.host", s, setS, erverHost)
 };
 
