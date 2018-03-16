1e94b8f25bf53fbfafcc9bcaf5f947a639b6181c
Fanda Vacek <vacek@elektroline.cz>
2018-03-16 09:54:36 +0100

    shvagent switches lublicator ON and OFF again

--- shvagent/src/theapp.cpp
+++ shvagent/src/theapp.cpp
@@ -37,13 +37,50 @@ TheApp::~TheApp()
 	shvInfo() << "destroying shv agent application";
 }
 
+enum class LublicatorStatus : unsigned
+{
+	PosOff      = 1 << 0,
+	PosOn       = 1 << 1,
+	PosMiddle   = 1 << 2,
+	PosError    = 1 << 3,
+	BatteryLow  = 1 << 4,
+	BatteryHigh = 1 << 5,
+	DoorOpenCabinet = 1 << 6,
+	DoorOpenMotor = 1 << 7,
+	ModeAuto    = 1 << 8,
+	ModeRemote  = 1 << 9,
+	ModeService = 1 << 10,
+	MainSwitch  = 1 << 11
+};
+
+std::string lublicatorStatusToString(unsigned st)
+{
+	std::string ret;
+	if(st & (unsigned)LublicatorStatus::PosOff) ret += " | PosOff";
+	if(st & (unsigned)LublicatorStatus::PosOn) ret += " | PosOn";
+	if(st & (unsigned)LublicatorStatus::PosMiddle) ret += " | PosMiddle";
+	if(st & (unsigned)LublicatorStatus::PosError) ret += " | PosError";
+	if(st & (unsigned)LublicatorStatus::BatteryLow) ret += " | BatteryLow";
+	if(st & (unsigned)LublicatorStatus::BatteryHigh) ret += " | BatteryHigh";
+	if(st & (unsigned)LublicatorStatus::DoorOpenCabinet) ret += " | DoorOpenCabinet";
+	if(st & (unsigned)LublicatorStatus::DoorOpenMotor) ret += " | DoorOpenMotor";
+	if(st & (unsigned)LublicatorStatus::ModeAuto) ret += " | ModeAuto";
+	if(st & (unsigned)LublicatorStatus::ModeRemote) ret += " | ModeRemote";
+	if(st & (unsigned)LublicatorStatus::ModeService) ret += " | ModeService";
+	if(st & (unsigned)LublicatorStatus::MainSwitch) ret += " | MainSwitch";
+	return ret;
+}
+
 static void print_children(shv::iotqt::rpc::ClientConnection *rpc, const std::string &path, int indent)
 {
 	//shvInfo() << "\tcall:" << "get" << "on shv path:" << shv_path;
 	cp::RpcResponse resp = rpc->callShvMethodSync(path, cp::Rpc::METH_GET);
 	shvDebug() << "\tgot response:" << resp.toCpon();
-	if(resp.isError())
-		throw shv::core::Exception(resp.error().message());
+	if(resp.isError()) {
+		//throw shv::core::Exception(resp.error().message());
+		shvError() << resp.error().message();
+		return;
+	}
 	shv::chainpack::RpcValue result = resp.result();
 	if(result.isList()) {
 		const cp::RpcValue::List list = resp.result().toList();
@@ -51,6 +88,10 @@ static void print_children(shv::iotqt::rpc::ClientConnection *rpc, const std::st
 			std::string s = dir.toString();
 			if(s.empty()) {
 				shvError() << "empty dir name";
+			}
+			else if(s == "odpojovace") {
+				shvInfo() << "skipping dir:" << s;
+				continue;
 			}
 			else {
 				std::string path2 = path + '/' + s;
@@ -60,7 +101,7 @@ static void print_children(shv::iotqt::rpc::ClientConnection *rpc, const std::st
 		}
 	}
 	else {
-		shvInfo() << std::string(indent, ' ') << result.toCpon();
+		shvInfo() << std::string(indent, ' ') << result.toCpon() << lublicatorStatusToString(result.toUInt());
 	}
 }
 
@@ -73,11 +114,36 @@ void TheApp::onBrokerConnectedChanged(bool is_connected)
 		shvInfo() << "   Lublicator Testing";
 		shvInfo() << "==================================================";
 		shv::iotqt::rpc::ClientConnection *rpc = m_rpcConnection;
-		{
+		if(0) {
 			shvInfo() << "------------ read shv tree";
 			print_children(rpc, "", 0);
 			//shvInfo() << "GOT:" << shv_path;
 		}
+		bool on = 0;
+		bool off = !on;
+		{
+			std::string shv_path = "/test/shv/lublicator2/";
+			shvInfo() << "------------ get STATUS";
+			cp::RpcResponse resp = rpc->callShvMethodSync(shv_path + "status", cp::Rpc::METH_GET);
+			shvInfo() << "\tgot response:" << resp.toCpon();
+			shv::chainpack::RpcValue result = resp.result();
+			shvInfo() << result.toCpon() << lublicatorStatusToString(result.toUInt());
+			on = !(result.toUInt() & (unsigned)LublicatorStatus::PosOn);
+			off = !on;
+		}
+		if(on) {
+			shvInfo() << "------------ switch ON";
+			std::string shv_path = "/test/shv/lublicator2/";
+			cp::RpcResponse resp = rpc->callShvMethodSync(shv_path + "cmdOn", cp::Rpc::METH_SET, true);
+			shvInfo() << "\tgot response:" << resp.toCpon();
+		}
+		if(off) {
+			shvInfo() << "------------ switch OFF";
+			std::string shv_path = "/test/shv/lublicator2/";
+			cp::RpcResponse resp = rpc->callShvMethodSync(shv_path + "cmdOff", cp::Rpc::METH_SET, true);
+			shvInfo() << "\tgot response:" << resp.toCpon();
+		}
+		return;
 		{
 			shvInfo() << "------------ get battery level";
 			std::string shv_path_lubl = "/test/shv/eu/pl/lublin/odpojovace/15/";
@@ -134,5 +200,10 @@ void TheApp::onRpcMessageReceived(const shv::chainpack::RpcMessage &msg)
 	else if(msg.isNotify()) {
 		cp::RpcNotify nt(msg);
 		shvInfo() << "RPC notify received:" << nt.toCpon();
+		if(nt.method() == cp::Rpc::METH_VAL_CHANGED) {
+			if(nt.shvPath() == "/test/shv/lublicator2/status") {
+				shvInfo() << lublicatorStatusToString(nt.params().toUInt());
+			}
+		}
 	}
 }
