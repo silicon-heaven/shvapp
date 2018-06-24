5153fd9200ecbc162b143748d565c305255ada65
Fanda Vacek <vacek@elektroline.cz>
2018-06-24 12:20:48 +0200

    Update build-win.cmd

--- build-win.cmd
+++ build-win.cmd
@@ -5,4 +5,5 @@ C:/Qt/5.8/mingw53_32/bin/qmake.exe DEFINES+=GIT_COMMIT=%CI_COMMIT_SHA% DEFINES+=
 C:/Qt\Tools\mingw530_32\bin\mingw32-make.exe -j4 || exit /b 2
 
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "/SdefaultSignTool=C:\Program Files (x86)\Windows Kits\10\bin\x86\signTool.exe sign /f C:\Certificates\Elektroline_code_signing.p12 /p 123456789asdfghjkl /t http://timestamp.comodoca.com/authenticode $f" shvspy/shvspy.iss  || exit /b 2
+"C:\Program Files (x86)\Inno Setup 5\iscc.exe" "/SdefaultSignTool=C:\Program Files (x86)\Windows Kits\10\bin\x86\signTool.exe sign /f C:\Certificates\Elektroline_code_signing.p12 /p 123456789asdfghjkl /t http://timestamp.comodoca.com/authenticode $f" clients/bfsview/bfsview.iss  || exit /b 2
 
