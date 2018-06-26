82a40d7b2f452ca9942a79c60db25fe3b1126548
Fanda Vacek <vacek@elektroline.cz>
2018-06-26 18:55:20 +0200

    Update deploy-win.cmd

--- deploy-win.cmd
+++ deploy-win.cmd
@@ -2,4 +2,8 @@
 
 IF NOT EXIST \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG% mkdir \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG%
 copy _inno\shvspy\* \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG%   || exit /b 2
+
+IF NOT EXIST \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG% mkdir \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%
+copy _inno\shvspy\* \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%   || exit /b 2
+
 @echo "copied."
