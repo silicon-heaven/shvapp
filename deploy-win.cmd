17f2857355a4d8471765eb9dd30d48cb1c103e40
Fanda Vacek <vacek@elektroline.cz>
2018-10-02 18:06:19 +0200

    Update deploy-win.cmd

--- deploy-win.cmd
+++ deploy-win.cmd
@@ -1,9 +1,13 @@
 @echo "deploy shv" %WORKSPACE%
 
+@echo "copying" _inno\shvspy\* to \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG%
+
 IF NOT EXIST \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG% mkdir \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG%
 copy _inno\shvspy\* \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG%   || exit /b 2
 
-IF NOT EXIST \\claudius.elektroline.cz\install\shvspy\%CI_COMMIT_REF_SLUG% mkdir \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%
-copy _inno\shvspy\* \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%   || exit /b 2
+@echo "copying" _inno\bfsview\* to \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%
+
+IF NOT EXIST \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG% mkdir \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%
+copy _inno\bfsview\* \\claudius.elektroline.cz\install\bfsview\%CI_COMMIT_REF_SLUG%   || exit /b 2
 
 @echo "copied."
