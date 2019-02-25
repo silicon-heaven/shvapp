bddda7c0a4ac6b35a36f3513172bbfafa8f146b9
Fanda Vacek <vacek@elektroline.cz>
2019-02-25 21:49:14 +0100

    jn50view: default shv path changed

--- clients/jn50view/src/appclioptions.cpp
+++ clients/jn50view/src/appclioptions.cpp
@@ -4,5 +4,5 @@ namespace cp = shv::chainpack;
 
 AppCliOptions::AppCliOptions()
 {
-	addOption("shvconv.shvPath").setType(cp::RpcValue::Type::String).setNames("-t", "--conv-shv-path").setDefaultValue("test/cze/plz/convjn50");
+	addOption("shvconv.shvPath").setType(cp::RpcValue::Type::String).setNames("-t", "--conv-shv-path").setDefaultValue("shv/cze/plz/pow/convjn50/convjn50");
 }
