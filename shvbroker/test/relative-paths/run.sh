7b91f1412890afcd97dcb9751f259697bd67a9c3
Fanda Vacek <vacek@elektroline.cz>
2019-03-18 10:57:36 +0100

    shvspy: ShvBrokerNodeItem::brokerId()

--- shvbroker/test/relative-paths/run.sh
+++ shvbroker/test/relative-paths/run.sh
@@ -4,6 +4,7 @@ BINDIR=/home/fanda/proj/_build/shv/bin
 SRCDIR=/home/fanda/proj/shv
 
 BROKER_TOPICS=rpcmsg,subscr #,sigres
+SLEEP_SETTLE=0.2
 
 # tmux setw remain-on-exit on
 
@@ -18,10 +19,11 @@ tmux split-window -h $BINDIR/shvbroker --config-dir $SRCDIR/shvbroker/test/relat
 #tmux split-window -h $BINDIR/revitestdevice -p 3755 -u iot --password lub42DUB --lt plain -m test/slave/lub1 --hbi 0
 # sleep 1
 
+sleep $SLEEP_SETTLE
 tmux select-pane -t0
 tmux split-window -h $BINDIR/revitestdevice -p 3756 -u iot --password lub42DUB --lt plain -m test/slave/lub1 --hbi 0 -n 3
 
-sleep 0.3
+sleep $SLEEP_SETTLE
 
 # tmux split-window $BINDIR/revitestdevice -p 3757 -u iot --password lub42DUB --lt plain -m test/slave/lub2 --hbi 0 -n 4 -c '[["../../../broker1/slave/lub1/1/status", "get"], [".broker/app", "subscribe", {"method":"chng", "path":"test"}], ["../lub2/3/status", "sim_set", 41]]' -v rpcmsg
 tmux split-window $BINDIR/revitestdevice -p 3757 -u iot --password lub42DUB --lt plain -m test/slave/lub2 --hbi 0 -n 4 -f $SRCDIR/shvbroker/test/relative-paths/test-calls.cpon -v testcalls
