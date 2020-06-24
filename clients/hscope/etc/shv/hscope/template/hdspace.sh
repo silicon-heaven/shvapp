e6c0e43074d4f6176403baa57cfcaf69058dc2e7
Fanda Vacek <vacek@elektroline.cz>
2020-06-24 21:41:22 +0200

    hscope: memfree

--- clients/hscope/etc/shv/hscope/template/hdspace.sh
+++ clients/hscope/etc/shv/hscope/template/hdspace.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 if [[ ! -z $MOUNT_POINT ]]; then
-	DEV=$MOUNT_POINT
-	DF_VAL=`df | grep "${MOUNT_POINT}$" | sed 's/ \+/ /g; s/%//g' | cut -d ' ' -f 5 `
+    DEV=$MOUNT_POINT
+    DF_VAL=`df | grep "${MOUNT_POINT}$" | sed 's/ \+/ /g; s/%//g' | cut -d ' ' -f 5 `
 elif [[ ! -z $DEVICE_NAME ]]; then
-	DEV=$DEVICE_NAME
-	DF_VAL=`df | grep "^${DEVICE_NAME}\s" | sed 's/ \+/ /g; s/%//g' | cut -d ' ' -f 5 `
+    DEV=$DEVICE_NAME
+    DF_VAL=`df | grep "^${DEVICE_NAME}\s" | sed 's/ \+/ /g; s/%//g' | cut -d ' ' -f 5 `
 fi
 
 # DF_VAL=90
 # echo $PROC
 if [[ -z $DF_VAL ]]; then
-	VAL=ERR
+    VAL=ERR
 elif [[ $DF_VAL -ge ${ERROR_TRESHOLD:-90} ]]; then
-	VAL=ERR
+    VAL=ERR
 elif [[ $DF_VAL -ge ${WARN_TRESHOLD:-75} ]]; then
-	VAL=WARN
+    VAL=WARN
 else
-	VAL=OK
+    VAL=OK
 fi
 
 if [[ -z $DF_VAL ]]; then
-	MSG="Invalid input parameters."
+    MSG="Invalid input parameters."
 else
-	MSG="Disc fill on ${DEV} is ${DF_VAL}%"
+    MSG="Disc fill on ${DEV} is ${DF_VAL}%"
 fi
 
 echo "{\"val\":\"$VAL\", \"msg\":\"$MSG\"}"
---
+++ clients/hscope/etc/shv/hscope/template/memfree.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MEM=`free | grep "Mem:" | sed 's/ \+/ /g' `
+USED_VAL=`echo $MEM | cut -d ' ' -f 3 `
+TOTAL_VAL=`echo $MEM | cut -d ' ' -f 2 `
+USED_PERCENT=$(( $USED_VAL * 100 / $TOTAL_VAL ))
+
+if [[ -z $USED_PERCENT ]]; then
+    VAL=ERR
+elif [[ $USED_PERCENT -ge ${ERROR_TRESHOLD:-90} ]]; then
+    VAL=ERR
+elif [[ $USED_PERCENT -ge ${WARN_TRESHOLD:-75} ]]; then
+    VAL=WARN
+else
+    VAL=OK
+fi
+
+MSG="Used memory ${USED_VAL} of ${TOTAL_VAL} ${USED_PERCENT}%"
+
+echo "{\"val\":\"$VAL\", \"msg\":\"$MSG\"}"
+
+
