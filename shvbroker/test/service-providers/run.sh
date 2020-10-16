76ca61e68f760cddcf831b0cad8c6411c1125bca
Jaroslav Beran <jberan@elektroline.cz>
2020-10-16 11:25:18 +0200

    shvbroker: adjust tests for secured connections
    A self-signed certificate is provided just for testing purposes.
    Validity of the certificate is for 10 years. It can be regenerated
    by using this command:
    
       openssl req -x509 -nodes -newkey rsa:4096 -keyout shvbroker_test.key
    -out shvbroker_test.cer -days 3650
    
    Do not forget to disable peer verification in the clients (--peer-verify
    false)!

--- shvbroker/test/service-providers/run.sh
+++ shvbroker/test/service-providers/run.sh
@@ -2,12 +2,21 @@
 
 TSTDIR=`dirname "$(readlink -f "$0")"`
 # echo TSTDIR: $TSTDIR
-BINDIR=/home/fanda/proj/_build/shv/bin
+# BINDIR=/home/fanda/proj/_build/shv/bin
 # SRCDIR=/home/fanda/proj/shv
 CFGDIR=$TSTDIR/../etc/broker
 
 BROKER_TOPICS=rpcmsg,subscr,ServiceProviders,SigRes,AclResolve
 SLEEP_SETTLE=0.5
+
+if [[ -z "$BINDIR" ]]
+then
+	echo 'Please set the BINDIR variable to the directory with SHV binaries on this system'
+	echo ''
+	echo 'Usage: BINDIR="/path/to/shv/binaries/" ./run.sh'
+	echo ''
+	exit 1
+fi
 
 # tmux set remain-on-exit on
 tmux new-window sh -c "$BINDIR/shvbroker --config-dir $CFGDIR/master/ -v $BROKER_TOPICS || read a"
@@ -26,7 +35,7 @@ tmux split-window sh -c "$BINDIR/shvbroker --config-dir $CFGDIR/slaveA/ -v $BROK
 sleep $SLEEP_SETTLE
 
 tmux select-pane -t0
-tmux split-window -hf $BINDIR/shvagent -p 3755 -u iot --password iotpwd --lt plain -m test/sites/broker1/brokerA/agentB2 --tester -v tester
+tmux split-window -hf $BINDIR/shvagent -p 37555 --sec-type ssl --peer-verify false -u iot --password iotpwd --lt plain -m test/sites/broker1/brokerA/agentB2 --tester -v tester
 
 sleep $SLEEP_SETTLE
 
@@ -36,6 +45,6 @@ sleep $SLEEP_SETTLE
 sleep $SLEEP_SETTLE
 
 # tmux split-window $BINDIR/revitestdevice -p 3757 -u iot --password iotpwd --lt plain -m test/slave/lub2 -n 4 -c '[["../../../broker1/slave/lub1/1/status", "get"], [".broker/app", "subscribe", {"method":"chng", "path":"test"}], ["../lub2/3/status", "sim_set", 41]]' -v rpcmsg
-tmux split-window $BINDIR/shvagent -p 3757 -u iot --password iotpwd --lt plain -m test/agentB2 --ts $TSTDIR/tests.cpon -v tester,rpcmsg
+tmux split-window $BINDIR/shvagent -p 3757 --sec-type none -u iot --password iotpwd --lt plain -m test/agentB2 --ts $TSTDIR/tests.cpon -v tester,rpcmsg
 
 # tmux select-layout tiled
