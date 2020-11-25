07ba9f14690fc71835e2cfa97973ac83cea6a2b2
Martin Špirk <spirk@elektroline.cz>
2020-11-25 14:41:02 +0100

    shvspy move to separate project https://github.com/silicon-heaven/shvspy

--- shvspy/src/appclioptions.cpp
+++
@@ -1,8 +0,0 @@
-#include "appclioptions.h"
-
-namespace cp = shv::chainpack;
-
-AppCliOptions::AppCliOptions()
-{
-	addOption("rpc.metaTypeExplicit").setType(cp::RpcValue::Type::Bool).setNames("--mtid", "--rpc-metatype-explicit").setComment("RpcMessage Type ID is included in RpcMessage when set, for more verbose -v rpcmsg log output").setDefaultValue(false);
-}
