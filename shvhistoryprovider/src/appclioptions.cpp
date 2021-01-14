16ad58a6dbe276a29a7b7b550fa38edc7839f5a6
Martin Špirk <spirk@elektroline.cz>
2021-01-14 11:30:28 +0100

    set default cacheSizeLimit for HP

--- shvhistoryprovider/src/appclioptions.cpp
+++ shvhistoryprovider/src/appclioptions.cpp
@@ -17,5 +17,5 @@ AppCliOptions::AppCliOptions()
 			.setComment("runs in test mode, shv/test/.. asks from elesys as \"normal\", otherwise doesn't care about them")
 			.setDefaultValue(false);
 	addOption("app.cacheSizeLimit").setType(shv::chainpack::RpcValue::Type::String).setNames("--cache-size-limit")
-			.setComment("set cache size limit (suffixes: k, M, G)").setDefaultValue("0");
+			.setComment("set cache size limit (suffixes: k, M, G)").setDefaultValue("10G");
 }
