71267a6b160567c3839eed6334ca00bdf8ecfc6c
Martin Špirk <spirk@elektroline.cz>
2021-04-14 15:56:46 +0200

    support data change domains in the dirty log

--- shvhistoryprovider/src/dirtylogmanager.h
+++ shvhistoryprovider/src/dirtylogmanager.h
@@ -23,9 +23,9 @@ private:
 
 	void insertDataMissingToDirtyLog();
 	void insertDataMissingToDirtyLog(const QString &shv_path);
-	void writeDirtyLog(const QString &shv_path, const QString &path, shv::chainpack::RpcValue value, std::string domain, bool is_connected);
+	void writeDirtyLog(const QString &shv_path, const QString &path, const shv::chainpack::RpcValue &value, int64_t timestamp, std::string domain, bool is_connected);
 	void checkDirtyLog(const QString &shv_path, bool is_connected);
-	void onDeviceDataChanged(const QString &path, const QString &method, const shv::chainpack::RpcValue &data);
+	void onDeviceDataChanged(const QString &path, const QString &method, shv::chainpack::RpcValue data);
 
 	ShvSubscription *m_chngSubscription;
 };
