0e4948d107b0d028d12718e32c6f3076baa67092
Martin Špirk <spirk@elektroline.cz>
2021-09-01 16:53:40 +0200

    SP: merging config file also with metadata

--- shvsitesprovider/src/main.cpp
+++ shvsitesprovider/src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("Elektroline");
 	QCoreApplication::setOrganizationDomain("elektroline.cz");
 	QCoreApplication::setApplicationName("shvsitesprovider");
-	QCoreApplication::setApplicationVersion("1.0.12");
+	QCoreApplication::setApplicationVersion("1.0.13");
 
 	std::vector<std::string> shv_args = NecroLog::setCLIOptions(argc, argv);
 
