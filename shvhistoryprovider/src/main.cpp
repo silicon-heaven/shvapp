776d26e73e94e4a0074122b7f70cc2482436f9dd
Martin Špirk <spirk@elektroline.cz>
2021-09-01 21:53:07 +0200

    HP: fixed until in getLog

--- shvhistoryprovider/src/main.cpp
+++ shvhistoryprovider/src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("Elektroline");
 	QCoreApplication::setOrganizationDomain("elektroline.cz");
 	QCoreApplication::setApplicationName("shvhistoryprovider");
-	QCoreApplication::setApplicationVersion("2.4.15");
+	QCoreApplication::setApplicationVersion("2.4.16");
 
 	std::vector<std::string> shv_args = NecroLog::setCLIOptions(argc, argv);
 	int ret = 0;
