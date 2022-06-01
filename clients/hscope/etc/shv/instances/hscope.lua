562653624c747c92550d0ea0ba04fd4986b68a09
Václav Kubernát <vkubernat@elektroline.cz>
2022-06-01 15:18:07 +0200

    hscope: Fix telegram example
    Unfortunately the solution with hscope_initializing won't work, because
    all the code in lua that deals with rpc_calls happen after the
    initialization. I have opted for a simple solution in lua: just skip the
    first status update.

--- clients/hscope/etc/shv/instances/hscope.lua
+++ clients/hscope/etc/shv/instances/hscope.lua
@@ -7,14 +7,18 @@ shv.log_info(shv_utils.print_table(_ENV))
 shv.log_info("I'm setting something for the child environments!")
 something = 123
 
+local enabled_paths = {}
+
 shv.on_broker_connected(function ()
     shv.rpc_call(".broker/currentClient", "mountPoint", "", function (value)
         shv.subscribe(value.value .. "/" .. cur_shv_path, "chng", function (path, new_value)
-            if not shv.hscope_initializing then -- We want to skip the initial status.
+            if enabled_paths[path] then
                 if new_value.value.severity.value ~= 'ok' then
                     -- bot:sendMessage(CHAT_ID, string.format('Test failure occurred.\nPath: %s\nSeverity: %s\nMessage: %s', path, new_value.value.severity.value, new_value.value.message.value))
                 end
             end
+
+            enabled_paths[path] = true
         end)
     end)
 end)
