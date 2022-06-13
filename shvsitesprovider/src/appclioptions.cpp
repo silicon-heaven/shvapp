edaa0778c7457c405b9f20e76084c70688d1b5b3
Václav Kubernát <vkubernat@elektroline.cz>
2022-06-13 10:55:57 +0200

    shvsitesprovider: Change default for localSitesDir
    With the old default, localSitesDir and the directory shvsitesprovider
    uses for cloning the git directory are the same. This is how
    shvsitesprovider roughly works:
    1) Remove /tmp/sites (which is by default localSitesDir - it could
       remove your local sites).
    2) git clone a repository named "sites" - this creates the /tmp/sites
       directory.
    3) Merge localSitesDir and the git repository (that are be default the
       same, so nothing happens in that case).
    4) Remove the /tmp/sites dir.
    5) Create the "sites/" shv structure from localSitesDir. By default,
       that's /tmp/sites, that's been deleted by the previous step, so
       nothing will be created.
    
    I have the changed localSitesDir to be /tmp/sites-local, so that there
    isn't a collision.

--- shvsitesprovider/src/appclioptions.cpp
+++ shvsitesprovider/src/appclioptions.cpp
@@ -4,7 +4,7 @@ AppCliOptions::AppCliOptions()
 {
 	addOption("app.localSitesDir").setType(shv::chainpack::RpcValue::Type::String).setNames("--lsd", "--local-sites-dir")
 			.setComment("Local file system directory, which contains shv config files.")
-			.setDefaultValue("/tmp/sites");
+			.setDefaultValue("/tmp/sites-local");
 	addOption("app.remoteSitesUrl").setType(shv::chainpack::RpcValue::Type::String).setNames("--rsu", "--remote-sites-url")
 			.setComment("Url location to remote server file which contains all Elektroline sites.")
 			.setDefaultValue("https://gitlab+deploy-token-478855:uQMhvdiGpd-6FmUAKnQU@gitlab.com/revitest-predator/sites");
