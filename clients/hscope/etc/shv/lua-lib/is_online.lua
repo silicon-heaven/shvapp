1e810881815525a47f902bc36c915bcc75a0a8e8
Václav Kubernát <vkubernat@elektroline.cz>
2022-06-13 17:23:09 +0200

    hscope: is_online: Don't run manually
    hscope runs all tests manually at the start, so no need to run it
    ourselves.

--- clients/hscope/etc/shv/lua-lib/is_online.lua
+++ clients/hscope/etc/shv/lua-lib/is_online.lua
@@ -32,14 +32,10 @@ return function (set_status, path)
 		shv.subscribe(path, 'mntchng', function (_, value)
 			do_set_status(set_status, value.value)
 		end)
-
-		-- We refresh the online status once at the beginning to populate the status field.
-		manual_refresh(set_status, path)
 	end)
 
-	-- Even though we have called manual_refresh once and we setup a mntchng subscribe, we can still allow the user to
-	-- refresh the online status manually (for whatever reason). Also, we need to return SOMETHING, otherwise hscope,
-	-- won't recognize this as a tester node.
+	-- Even though we setup a mntchng subscribe, we can still allow the user to refresh the online status manually (for
+	-- whatever reason). Also, we need to return SOMETHING, otherwise hscope won't recognize this as a tester node.
 	return function ()
 		manual_refresh(set_status, path)
 	end
