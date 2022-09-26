8f1bfc315ea04e83de680d2c3b7c5dfc1fe29da8
Václav Kubernát <vkubernat@elektroline.cz>
2022-09-26 14:49:25 +0200

    history: Add units to options

--- historyprovider/src/appclioptions.cpp
+++ historyprovider/src/appclioptions.cpp
@@ -9,9 +9,9 @@ AppCliOptions::AppCliOptions()
 	addOption("app.journalCacheSizeLimit").setType(shv::chainpack::RpcValue::Type::String).setNames("--journal-cache-size-limit")
 			.setComment("Set journal cache size limit (suffixes: k, M, G)").setDefaultValue("1G");
 	addOption("app.journalSanitizerInterval").setType(shv::chainpack::RpcValue::Type::Int).setNames("--journal-sanitizer-interval")
-			.setComment("Set journal sanitizer interval").setDefaultValue(60);
+			.setComment("Set journal sanitizer interval in seconds").setDefaultValue(60);
 	addOption("app.logMaxAge").setType(shv::chainpack::RpcValue::Type::Int).setNames("--log-max-age")
-			.setComment("Set log max age: dirty log age or timer-based syncing for (master) pushlog devices").setDefaultValue(60 /*seconds*/ * 60 /*minutes*/ * 1 /*hours*/); // What's a correct value? An hour?
+			.setComment("Set log max age in seconds: dirty log age or timer-based syncing for (master) pushlog devices").setDefaultValue(60 /*seconds*/ * 60 /*minutes*/ * 1 /*hours*/); // What's a correct value? An hour?
 	addOption("app.cacheInitMaxAge").setType(shv::chainpack::RpcValue::Type::Int).setNames("--cache-init-max-age")
-			.setComment("Set max age for initial cache sync").setDefaultValue(60 /*seconds*/ * 60 /*minutes*/ * 24 /*hours*/ * 30 /*days*/); // What's a correct value? An hour?
+			.setComment("Set max age in seconds for initial cache sync").setDefaultValue(60 /*seconds*/ * 60 /*minutes*/ * 24 /*hours*/ * 30 /*days*/); // What's a correct value? An hour?
 }
