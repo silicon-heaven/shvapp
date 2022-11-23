cc22d4e0a1b5f0d6d2acbc70b46ef8250928e8c5
Václav Kubernát <vkubernat@elektroline.cz>
2022-11-23 16:19:24 +0100

    history: getlog: Add test for pattern matching

--- historyprovider3/tests/getlog.cpp
+++ historyprovider3/tests/getlog.cpp
@@ -52,47 +52,104 @@ auto make_entry(const std::string& timestamp, const std::string& path, const Rpc
 DOCTEST_TEST_CASE("getLog")
 {
 	NecroLog::setTopicsLogTresholds("GetLog:D,ShvJournal:D");
-    std::vector<int64_t> expected_timestamps;
     shv::core::utils::ShvGetLogParams get_log_params;
 	std::filesystem::remove_all(journal_dir);
 	std::filesystem::create_directories(journal_dir);
 
-    std::vector<std::function<shv::core::utils::ShvJournalFileReader()>> readers {
-		create_reader({
-			make_entry("2022-07-07T18:06:15.557Z", "APP_START", true, false),
-			make_entry("2022-07-07T18:06:17.784Z", "zone1/system/sig/plcDisconnected", false, false),
-			make_entry("2022-07-07T18:06:17.784Z", "zone1/zone/Zone1/plcDisconnected", false, false),
-			make_entry("2022-07-07T18:06:17.869Z", "zone1/pme/TSH1-1/switchRightCounterPermanent", 0U, false),
-		}),
-		create_reader({
-			make_entry("2022-07-07T18:06:17.872Z", "zone1/system/sig/plcDisconnected", false, false),
-			make_entry("2022-07-07T18:06:17.874Z", "zone1/zone/Zone1/plcDisconnected", false, false),
-			make_entry("2022-07-07T18:06:17.880Z", "zone1/pme/TSH1-1/switchRightCounterPermanent", 0U, false),
-		})
-	};
+	DOCTEST_SUBCASE("filtering")
+	{
+		std::vector<std::function<shv::core::utils::ShvJournalFileReader()>> readers {
+			create_reader({
+				make_entry("2022-07-07T18:06:15.557Z", "APP_START", true, false),
+				make_entry("2022-07-07T18:06:17.784Z", "zone1/system/sig/plcDisconnected", false, false),
+				make_entry("2022-07-07T18:06:17.784Z", "zone1/zone/Zone1/plcDisconnected", false, false),
+				make_entry("2022-07-07T18:06:17.869Z", "zone1/pme/TSH1-1/switchRightCounterPermanent", 0U, false),
+			}),
+			create_reader({
+				make_entry("2022-07-07T18:06:17.872Z", "zone1/system/sig/plcDisconnected", false, false),
+				make_entry("2022-07-07T18:06:17.874Z", "zone1/zone/Zone1/plcDisconnected", false, false),
+				make_entry("2022-07-07T18:06:17.880Z", "zone1/pme/TSH1-1/switchRightCounterPermanent", 0U, false),
+			})
+		};
 
-    DOCTEST_SUBCASE("default params")
-    {
-        expected_timestamps = {1657217175557, 1657217177784, 1657217177784, 1657217177869, 1657217177872, 1657217177874, 1657217177880};
-    }
+		DOCTEST_SUBCASE("since/until")
+		{
+			std::vector<int64_t> expected_timestamps;
 
-    DOCTEST_SUBCASE("since")
-    {
-        expected_timestamps = {1657217177872, 1657217177874, 1657217177880};
-        get_log_params.since = RpcValue::DateTime::fromMSecsSinceEpoch(1657217177872);
-    }
+			DOCTEST_SUBCASE("default params")
+			{
+				expected_timestamps = {1657217175557, 1657217177784, 1657217177784, 1657217177869, 1657217177872, 1657217177874, 1657217177880};
+			}
 
-    DOCTEST_SUBCASE("until")
-    {
-        expected_timestamps = {1657217175557, 1657217177784, 1657217177784, 1657217177869};
-        get_log_params.until = RpcValue::DateTime::fromMSecsSinceEpoch(1657217177870);
-    }
-    std::vector<int64_t> actual_timestamps;
-    shv::core::utils::ShvMemoryJournal entries;
-    entries.loadLog(getLog(readers, get_log_params));
-    for (const auto& entry : entries.entries()) {
-        actual_timestamps.push_back(entry.epochMsec);
-    }
+			DOCTEST_SUBCASE("since")
+			{
+				expected_timestamps = {1657217177872, 1657217177874, 1657217177880};
+				get_log_params.since = RpcValue::DateTime::fromMSecsSinceEpoch(1657217177872);
+			}
 
-    REQUIRE(actual_timestamps == expected_timestamps);
+			DOCTEST_SUBCASE("until")
+			{
+				expected_timestamps = {1657217175557, 1657217177784, 1657217177784, 1657217177869};
+				get_log_params.until = RpcValue::DateTime::fromMSecsSinceEpoch(1657217177870);
+			}
+			std::vector<int64_t> actual_timestamps;
+			shv::core::utils::ShvMemoryJournal entries;
+			entries.loadLog(getLog(readers, get_log_params));
+			for (const auto& entry : entries.entries()) {
+				actual_timestamps.push_back(entry.epochMsec);
+			}
+
+			REQUIRE(actual_timestamps == expected_timestamps);
+		}
+
+		DOCTEST_SUBCASE("pattern")
+		{
+			std::vector<std::string> expected_paths;
+
+			DOCTEST_SUBCASE("default params")
+			{
+				expected_paths = {
+					"APP_START",
+					"zone1/system/sig/plcDisconnected",
+					"zone1/zone/Zone1/plcDisconnected",
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+					"zone1/system/sig/plcDisconnected",
+					"zone1/zone/Zone1/plcDisconnected",
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+				};
+			}
+
+			DOCTEST_SUBCASE("exact path")
+			{
+				get_log_params.pathPattern = "zone1/pme/TSH1-1/switchRightCounterPermanent";
+				expected_paths = {
+					// There are two entries with this path
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+				};
+			}
+
+			DOCTEST_SUBCASE("wildcard")
+			{
+				get_log_params.pathPattern = "zone1/**";
+				expected_paths = {
+					"zone1/system/sig/plcDisconnected",
+					"zone1/zone/Zone1/plcDisconnected",
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+					"zone1/system/sig/plcDisconnected",
+					"zone1/zone/Zone1/plcDisconnected",
+					"zone1/pme/TSH1-1/switchRightCounterPermanent",
+				};
+			}
+
+			std::vector<std::string> actual_paths;
+			shv::core::utils::ShvMemoryJournal entries;
+			entries.loadLog(getLog(readers, get_log_params));
+			for (const auto& entry : entries.entries()) {
+				actual_paths.push_back(entry.path);
+			}
+
+			REQUIRE(actual_paths == expected_paths);
+		}
+	}
 }
