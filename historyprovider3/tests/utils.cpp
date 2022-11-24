49198583a1199fce526d4e430fa131ca1f9d65de
Václav Kubernát <vkubernat@elektroline.cz>
2022-11-24 20:33:38 +0100

    history: tests: Refactor utils

---
+++ historyprovider3/tests/utils.cpp
@@ -0,0 +1,56 @@
+#include "tests/utils.h"
+
+#include "src/historyapp.h"
+#include "src/appclioptions.h"
+
+QDir get_site_cache_dir(const std::string& site_path)
+{
+	using shv::core::Utils;
+	return QDir{QString::fromStdString(Utils::joinPath(Utils::joinPath(HistoryApp::instance()->cliOptions()->journalCacheRoot(), site_path), "_shvjournal"s))};
+}
+
+void remove_cache_contents(const std::string& site_path)
+{
+	auto cache_dir = get_site_cache_dir(site_path);
+	cache_dir.removeRecursively();
+	cache_dir.mkpath(".");
+}
+
+RpcValue::List get_cache_contents(const std::string& site_path)
+{
+	RpcValue::List res;
+	auto cache_dir = get_site_cache_dir(site_path);
+	cache_dir.setFilter(QDir::Files | QDir::NoDotAndDotDot);
+	auto it = QDirIterator(cache_dir, QDirIterator::Subdirectories);
+	while (it.hasNext()) {
+		auto entry = it.next();
+		RpcValue::List name_and_size;
+		name_and_size.push_back(entry.mid(cache_dir.path().size() + 1).toStdString());
+		QFile file(entry);
+		name_and_size.push_back(RpcValue::UInt(QFile(cache_dir.filePath(entry)).size()));
+		res.push_back(name_and_size);
+	}
+
+	std::sort(res.begin(), res.end(), [] (const RpcValue& a, const RpcValue& b) {
+		return a.asList().front().asString() < b.asList().front().asString();
+	});
+
+	return res;
+}
+
+void create_dummy_cache_files(const std::string& site_path, const std::vector<DummyFileInfo>& files)
+{
+	remove_cache_contents(site_path);
+	auto cache_dir = get_site_cache_dir(site_path);
+
+	for (const auto& file : files) {
+		QFile qfile(cache_dir.filePath(file.fileName));
+		qfile.open(QFile::WriteOnly);
+		qfile.write(file.content.c_str());
+	}
+}
+
+std::string join(const std::string& a, const std::string& b)
+{
+	return shv::core::Utils::joinPath(a, b);
+}
---
+++ historyprovider3/tests/utils.h
@@ -0,0 +1,181 @@
+#pragma once
+
+#include <shv/core/utils/shvjournalentry.h>
+#include <shv/core/utils/shvmemoryjournal.h>
+
+#include <QDir>
+#include <QDirIterator>
+
+namespace cp = shv::chainpack;
+using cp::RpcValue;
+using namespace std::string_literals;
+
+struct DummyFileInfo {
+	QString fileName;
+	std::string content;
+};
+QDir get_site_cache_dir(const std::string& site_path);
+void remove_cache_contents(const std::string& site_path);
+RpcValue::List get_cache_contents(const std::string& site_path);
+void create_dummy_cache_files(const std::string& site_path, const std::vector<DummyFileInfo>& files);
+std::string join(const std::string& a, const std::string& b);
+
+
+const auto dummy_logfile = R"(2022-07-07T18:06:15.557Z	809779	APP_START	true		SHV_SYS	0	
+2022-07-07T18:06:17.784Z	809781	zone1/system/sig/plcDisconnected	false		chng	2	
+2022-07-07T18:06:17.784Z	809781	zone1/zone/Zone1/plcDisconnected	false		chng	2	
+2022-07-07T18:06:17.869Z	809781	zone1/pme/TSH1-1/switchRightCounterPermanent	0u		chng	2	
+)"s;
+
+const auto dummy_logfile2 = R"(2022-07-07T18:06:17.872Z	809781	zone1/system/sig/plcDisconnected	false		chng	2	
+2022-07-07T18:06:17.874Z	809781	zone1/zone/Zone1/plcDisconnected	false		chng	2	
+2022-07-07T18:06:17.880Z	809781	zone1/pme/TSH1-1/switchRightCounterPermanent	0u		chng	2	
+)"s;
+
+const auto logfile_one_entry = R"(2022-07-07T18:06:17.872Z	809781	zone1/system/sig/plcDisconnected	false		chng	2	)"s;
+
+const auto dummy_pushlog = RpcValue::fromCpon(R"(
+<
+  "dateTime":d"2022-09-15T13:30:04.293Z",
+  "device":{"id":"historyprovider"},
+  "fields":[
+    {"name":"timestamp"},
+    {"name":"path"},
+    {"name":"value"},
+    {"name":"shortTime"},
+    {"name":"domain"},
+    {"name":"valueFlags"},
+    {"name":"userId"}
+  ],
+  "logParams":{"recordCountLimit":1000, "until":d"2022-07-07T18:06:17.870Z", "withPathsDict":true, "withSnapshot":false, "withTypeInfo":false},
+  "logVersion":2,
+  "pathsDict":i{1:"APP_START", 2:"zone1/system/sig/plcDisconnected", 3:"zone1/zone/Zone1/plcDisconnected", 4:"zone1/pme/TSH1-1/switchRightCounterPermanent"},
+  "recordCount":4,
+  "recordCountLimit":1000,
+  "recordCountLimitHit":false,
+  "since":d"2022-07-07T18:06:15.557Z",
+  "until":d"2022-07-07T18:06:15.557Z",
+  "withPathsDict":true,
+  "withSnapshot":false
+>[
+  [d"2022-07-07T18:06:15.557Z", 1, true, null, "SHV_SYS", 0u, null],
+]
+)");
+
+const auto dummy_pushlog3 = RpcValue::fromCpon(R"(
+<
+  "dateTime":d"2022-09-15T13:30:04.293Z",
+  "device":{"id":"historyprovider"},
+  "fields":[
+    {"name":"timestamp"},
+    {"name":"path"},
+    {"name":"value"},
+    {"name":"shortTime"},
+    {"name":"domain"},
+    {"name":"valueFlags"},
+    {"name":"userId"}
+  ],
+  "logParams":{"recordCountLimit":1000, "until":d"2022-07-07T18:06:17.870Z", "withPathsDict":true, "withSnapshot":false, "withTypeInfo":false},
+  "logVersion":2,
+  "pathsDict":i{1:"APP_START", 2:"zone1/system/sig/plcDisconnected", 3:"zone1/zone/Zone1/plcDisconnected", 4:"zone1/pme/TSH1-1/switchRightCounterPermanent"},
+  "recordCount":4,
+  "recordCountLimit":1000,
+  "recordCountLimitHit":false,
+  "since":d"2022-07-07T18:06:15.557Z",
+  "until":d"2022-07-07T18:06:15.557Z",
+  "withPathsDict":true,
+  "withSnapshot":false
+>[
+  [d"2022-07-07T18:06:15.557Z", 2, true, null, "SHV_SYS", 0u, null],
+]
+)");
+
+const auto dummy_pushlog2 = RpcValue::fromCpon(R"(
+<
+  "dateTime":d"2022-09-15T13:30:04.293Z",
+  "device":{"id":"historyprovider"},
+  "fields":[
+    {"name":"timestamp"},
+    {"name":"path"},
+    {"name":"value"},
+    {"name":"shortTime"},
+    {"name":"domain"},
+    {"name":"valueFlags"},
+    {"name":"userId"}
+  ],
+  "logParams":{"recordCountLimit":1000, "until":d"2022-07-07T18:06:17.870Z", "withPathsDict":true, "withSnapshot":false, "withTypeInfo":false},
+  "logVersion":2,
+  "pathsDict":i{1:"APP_START", 2:"zone1/system/sig/plcDisconnected", 3:"zone1/zone/Zone1/plcDisconnected", 4:"zone1/pme/TSH1-1/switchRightCounterPermanent"},
+  "recordCount":4,
+  "recordCountLimit":1000,
+  "recordCountLimitHit":false,
+  "since":d"2022-07-07T18:06:15.554Z",
+  "until":d"2022-07-07T18:06:15.554Z",
+  "withPathsDict":true,
+  "withSnapshot":false
+>[
+  [d"2022-07-07T18:06:15.554Z", 1, true, null, "SHV_SYS", 0u, null],
+]
+)");
+
+const auto dummy_getlog_response = RpcValue::fromCpon(R"(
+<
+  "dateTime":d"2022-09-15T13:30:04.293Z",
+  "device":{"id":"historyprovider"},
+  "fields":[
+    {"name":"timestamp"},
+    {"name":"path"},
+    {"name":"value"},
+    {"name":"shortTime"},
+    {"name":"domain"},
+    {"name":"valueFlags"},
+    {"name":"userId"}
+  ],
+  "logParams":{"recordCountLimit":1000, "until":d"2022-07-07T18:06:17.870Z", "withPathsDict":true, "withSnapshot":false, "withTypeInfo":false},
+  "logVersion":2,
+  "pathsDict":i{1:"APP_START", 2:"zone1/system/sig/plcDisconnected", 3:"zone1/zone/Zone1/plcDisconnected", 4:"zone1/pme/TSH1-1/switchRightCounterPermanent"},
+  "recordCount":4,
+  "recordCountLimit":1000,
+  "recordCountLimitHit":false,
+  "since":d"2022-07-07T18:06:15.557Z",
+  "until":d"2022-07-07T18:06:17.870Z",
+  "withPathsDict":true,
+  "withSnapshot":false
+>[
+  [d"2022-07-07T18:06:15.557Z", 1, true, null, "SHV_SYS", 0u, null],
+  [d"2022-07-07T18:06:17.784Z", 2, false, null, null, 2u, null],
+  [d"2022-07-07T18:06:17.784Z", 3, false, null, null, 2u, null],
+  [d"2022-07-07T18:06:17.869Z", 4, 0u, null, null, 2u, null]
+]
+)");
+
+const auto five_thousand_records_getlog_response = RpcValue::fromCpon((R"(
+<
+  "dateTime":d"2022-09-15T13:30:04.293Z",
+  "device":{"id":"historyprovider"},
+  "fields":[
+    {"name":"timestamp"},
+    {"name":"path"},
+    {"name":"value"},
+    {"name":"shortTime"},
+    {"name":"domain"},
+    {"name":"valueFlags"},
+    {"name":"userId"}
+  ],
+  "logParams":{"recordCountLimit":1000, "until":d"2022-07-07T18:06:17.870Z", "withPathsDict":true, "withSnapshot":false, "withTypeInfo":false},
+  "logVersion":2,
+  "pathsDict":i{1:"APP_START", 2:"zone1/system/sig/plcDisconnected", 3:"zone1/zone/Zone1/plcDisconnected", 4:"zone1/pme/TSH1-1/switchRightCounterPermanent"},
+  "recordCount":5000,
+  "recordCountLimit":1000,
+  "recordCountLimitHit":false,
+  "since":d"2022-07-07T18:06:15.557Z",
+  "until":d"2022-07-07T18:06:17.870Z",
+  "withPathsDict":true,
+  "withSnapshot":false
+>[
+  )" + QString(R"([d"2022-07-07T18:06:15.557Z", 1, true, null, "SHV_SYS", 0u, null],
+)").repeated(5000) + R"(]
+)").toStdString());
+
+const auto ls_size_true = RpcValue::fromCpon(R"({"size": true})");
+const auto read_offset_0 = RpcValue::fromCpon(R"({"offset":0})");
