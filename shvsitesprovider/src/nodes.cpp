4de7470eb8706c995ac13db835ee3f7e5b6c2eaf
Václav Kubernát <vkubernat@elektroline.cz>
2023-01-12 17:18:14 +0100

    qt6: Fix deprecated QCryptographicHash::addData

--- shvsitesprovider/src/nodes.cpp
+++ shvsitesprovider/src/nodes.cpp
@@ -229,7 +229,7 @@ cp::RpcValue AppRootNode::callMethodRq(const cp::RpcRequest &rq)
 	else if (method == METH_FILE_HASH) {
 		string bytes = readFile(rpcvalue_cast<QString>(rq.shvPath())).toString();
 		QCryptographicHash h(QCryptographicHash::Sha1);
-		h.addData(bytes.data(), static_cast<int>(bytes.size()));
+		h.addData(QByteArrayView(bytes.data(), static_cast<int>(bytes.size())));
 		return h.result().toHex().toStdString();
 	}
 	else if (method == cp::Rpc::METH_GET) {
