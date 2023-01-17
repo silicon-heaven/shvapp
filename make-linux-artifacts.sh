95192787f9e9b38c5038a6d48a6280d678db35c6
Václav Kubernát <vkubernat@elektroline.cz>
2023-01-17 11:42:39 +0100

    Revert "Fix build"
    This reverts commit 12974d684529f27178757249275eb974d3d4e7e0.

--- make-linux-artifacts.sh
+++ make-linux-artifacts.sh
@@ -4,7 +4,7 @@
 #APP_VER=1.0.0
 
 # QT_DIR=/home/fanda/programs/qt5/5.14.1/gcc_64
-QT_DIR=/opt/qt/6.4.2/gcc_64
+QT_DIR=/opt/qt/5.15.2/gcc_64
 QT_LIB_DIR=$QT_DIR/lib
 
 # BUILD_DIR=.
@@ -22,23 +22,23 @@ RSYNC='rsync -av --exclude *.debug'
 echo "$RSYNC lib bin $DIST_DIR"
 $RSYNC lib bin $DIST_DIR
 # exit 0
-$RSYNC $QT_LIB_DIR/libQt6Core.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Gui.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Widgets.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6XmlPatterns.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Network.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Sql.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Xml.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6Qml.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6Quick.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6Svg.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6WebSockets.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6ScriptTools.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6PrintSupport.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6SerialPort.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6DBus.so* $DIST_LIB_DIR
-#$RSYNC $QT_LIB_DIR/libQt6Multimedia.so* $DIST_LIB_DIR
-$RSYNC $QT_LIB_DIR/libQt6XcbQpa.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Core.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Gui.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Widgets.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5XmlPatterns.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Network.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Sql.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Xml.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5Qml.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5Quick.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5Svg.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5WebSockets.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5ScriptTools.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5PrintSupport.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5SerialPort.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5DBus.so* $DIST_LIB_DIR
+#$RSYNC $QT_LIB_DIR/libQt5Multimedia.so* $DIST_LIB_DIR
+$RSYNC $QT_LIB_DIR/libQt5XcbQpa.so* $DIST_LIB_DIR
 
 $RSYNC $QT_LIB_DIR/libicu*.so* $DIST_LIB_DIR
 
