23503212c1aab28d20458d306617a9961ef42763
Fanda Vacek <fvacek@elektroline.cz>
2024-02-12 17:20:56 +0100

    Remove shvcall from repo, it is in libshv already

--- shvcall/src/appclioptions.cpp
+++
@@ -1,9 +0,0 @@
-#include "appclioptions.h"
-
-AppCliOptions::AppCliOptions()
-{
-	addOption("path").setType(shv::chainpack::RpcValue::Type::String).setNames("--path").setComment("Shv call path");
-	addOption("method").setType(shv::chainpack::RpcValue::Type::String).setNames("--method").setComment("Shv call method");
-	addOption("params").setType(shv::chainpack::RpcValue::Type::String).setNames("--params").setComment("Shv call params");
-	addOption("isChainPackOutput").setType(shv::chainpack::RpcValue::Type::Bool).setNames("-x", "--chainpack-output").setComment("ChainPack output");
-}
