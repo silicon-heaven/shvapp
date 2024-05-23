ee2a884fa335ac2dcc3b3e48c2af91c591c10dda
Václav Kubernát <vkubernat@elektroline.cz>
2024-05-23 15:37:26 +0200

    Cleanup build-win.sh

--- build-win.sh
+++ build-win.sh
@@ -1,9 +1,10 @@
-echo "making SHV" $WORKSPACE
-export PATH=/c/mingw-12/x86_64-12.1.0-release-posix-seh-rt_v10-rev3/mingw64/bin:/c/Qt5/Tools/CMake_64/bin:$PATH
+set -euxo pipefail
+echo "making SHV ${WORKSPACE:-}"
+export PATH="/c/mingw-12/x86_64-12.1.0-release-posix-seh-rt_v10-rev3/mingw64/bin:/c/Qt5/Tools/CMake_64/bin:$PATH"
 export CXXFLAGS="-DGIT_COMMIT=${CI_COMMIT_SHA} -DGIT_BRANCH=${CI_COMMIT_REF_SLUG} -DBUILD_ID=${CI_PIPELINE_ID}"
 
 cmake.exe \
-    -DWITH_SHV_WEBSOCKETS=OFF \
+    -DLIBSHV_WITH_WEBSOCKETS=OFF \
     -G "MinGW Makefiles" \
     -DCMAKE_PREFIX_PATH=C:/Qt6/6.5.3/mingw_64 \
     -DCMAKE_INSTALL_PREFIX=. \
@@ -12,25 +13,20 @@ cmake.exe \
     -DUSE_QT6=ON \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache.exe \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache.exe \
-    . || exit 2
+    .
 
-cmake.exe --build . -j8 || exit 2
+cmake.exe --build . -j8
 
 # We're installing into the build directory, all the .iss scripts depend on that.
-cmake.exe --install . || exit 2
-
-JN50VIEW_VERSION=`grep APP_VERSION clients/jn50view/src/version.h | cut -d\" -f2`
-BFSVIEW_VERSION=`grep APP_VERSION clients/bfsview/src/version.h | cut -d\" -f2`
+cmake.exe --install .
 
 echo "making jn50view-doc"
-cd clients/jn50view/doc/help
-c:/Qt/Tools/mingw730_64/bin/mingw32-make.exe html || exit 2
-cd ../../../..
+pushd clients/jn50view/doc/help
+c:/Qt/Tools/mingw730_64/bin/mingw32-make.exe html
+pushd
 
 # TODO: Reenable these, if we're ever going to install this somewhere. Also port them to windeployqt!
 # "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "-DVERSION=${BFSVIEW_VERSION}" clients/bfsview/bfsview.iss || exit /b 2
 # "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "-DVERSION=${JN50VIEW_VERSION}" clients/jn50view/jn50view.iss || exit /b 2
-
-
-
-
+# JN50VIEW_VERSION=`grep APP_VERSION clients/jn50view/src/version.h | cut -d\" -f2`
+# BFSVIEW_VERSION=`grep APP_VERSION clients/bfsview/src/version.h | cut -d\" -f2`
